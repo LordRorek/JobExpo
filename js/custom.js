57e1129ee7df7af81b1023b6a35f36ff3379f313
LordRorek <jwchaley@aol.com>
2020-06-04 15:04:55 -0400

    Added forms to settings tabs

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,23 @@
-
 var chat = document.getElementById("chat");
+var personal = document.getElementById("item-1");
+var statement = $( "#settings" ).hasClass('active');
 
+$(document).ready(function(){
+  $( "#settings" ).click(function() {
+    setTimeout(check, 0.0000001)
+  });
+  function check(){
+    if (!statement) {
+      $(chat).addClass('delete');
+      $(personal).addClass('active');
+      $(document.getElementById("item-1-settings")).removeClass('delete');
+      $(document.getElementById("item-2-settings")).addClass('delete');
+      $(document.getElementById("item-3-settings")).addClass('delete');
+      $(document.getElementById("item-4-settings")).addClass('delete');
+      console.log("HELLO!");
+    };
+  };
+});
 $(document).ready(function() {
   $('.item-1').click(function() {
     var target = "#" + $(this).attr('class').split(' ')[0] + "-settings"
@@ -15,7 +32,6 @@ $(document).ready(function() {
     $(chat).addClass('delete');
     var target = "#" + $(this).attr('class').split(' ')[0] + "-settings"
     var siblings = $(this).siblings()
-    $(chat).addClass('delete');
     $(target).removeClass('delete');
     $(siblings).each(function() {
       var item = "#" + $(this).attr('class').split(' ')[0] + "-settings"
@@ -25,7 +41,6 @@ $(document).ready(function() {
   $('.item-3').click(function() {
     var target = "#" + $(this).attr('class').split(' ')[0] + "-settings"
     var siblings = $(this).siblings()
-    $(chat).addClass('delete');
     $(target).removeClass('delete');
     $(siblings).each(function() {
       var item = "#" + $(this).attr('class').split(' ')[0] + "-settings"
@@ -35,7 +50,6 @@ $(document).ready(function() {
   $('.item-4').click(function() {
     var target = "#" + $(this).attr('class').split(' ')[0] + "-settings"
     var siblings = $(this).siblings()
-    $(chat).addClass('delete');
     $(target).removeClass('delete');
     $(siblings).each(function() {
       var item = "#" + $(this).attr('class').split(' ')[0] + "-settings"
@@ -43,3 +57,9 @@ $(document).ready(function() {
     });
   });
 });
+
+function countChar(val) {
+  var len = val.value.length;
+  $('#current').text(len);
+
+};
